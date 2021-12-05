438b71f72311c689fabda7087b2b8e6f414012f1
Hiep <hiepltm.dn@gmail.com>
2021-12-05 16:28:38 +0700

    first update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import BaiTapThucHanhLayout from './BaiTapLayoutComponent/BaiTapThucHanhLayout';
+import BaiTapThuKinh from './BaiTapThuKinh/BaiTapThuKinh';
 
 function App() {
   return (
     <div className="App">
-      <BaiTapThucHanhLayout/>
+      <BaiTapThuKinh/>
 
     </div>
   );
